03a1de399392d12032cf9511dc454823da32f714
David MARQUET <marquetd@gmail.com>
2016-10-31 14:08:36 +0100

    Configuration cgtr

--- interface_config.js
+++ interface_config.js
@@ -14,10 +14,10 @@ var interfaceConfig = {// eslint-disable-line no-unused-vars
     SHOW_BRAND_WATERMARK: false,
     BRAND_WATERMARK_LINK: "",
     SHOW_POWERED_BY: false,
-    GENERATE_ROOMNAMES_ON_WELCOME_PAGE: true,
+    GENERATE_ROOMNAMES_ON_WELCOME_PAGE: false,
     APP_NAME: "SITM - Visio",
     LANG_DETECTION: true, // Allow i18n to detect the system language
-    INVITATION_POWERED_BY: true,
+    INVITATION_POWERED_BY: false,
     /**
      * If we should show authentication block in profile
      */
@@ -25,7 +25,7 @@ var interfaceConfig = {// eslint-disable-line no-unused-vars
     // the toolbar buttons line is intentionally left in one line, to be able
     // to easily override values or remove them using regex
     //MAIN_TOOLBAR_BUTTONS: ['microphone', 'camera', 'desktop', 'invite', 'fullscreen', 'hangup'], // jshint ignore:line
-    MAIN_TOOLBAR_BUTTONS: ['microphone', 'camera', 'invite', 'fullscreen', 'hangup'], // jshint ignore:line
+    MAIN_TOOLBAR_BUTTONS: ['microphone', 'camera', 'desktop', 'invite', 'fullscreen', 'hangup'], // jshint ignore:line
     /**
      * The index of the splitter button in the main toolbar. The splitter
      * button is a button in the toolbar that will be applied a special styling
@@ -33,7 +33,8 @@ var interfaceConfig = {// eslint-disable-line no-unused-vars
      */
     //MAIN_TOOLBAR_SPLITTER_INDEX: -1,
     //TOOLBAR_BUTTONS: ['profile', 'authentication', 'microphone', 'camera', 'desktop', 'recording', 'security', 'raisehand', 'chat', 'etherpad', 'sharedvideo', 'sip', 'dialpad', 'settings', 'hangup', 'filmstrip', 'contacts'], // jshint ignore:line
-    TOOLBAR_BUTTONS: ['profile', 'microphone', 'camera', 'security', 'chat', 'sip', 'dialpad', 'settings', 'hangup', 'filmstrip'], // jshint ignore:line
+    TOOLBAR_BUTTONS: ['authentication', 'microphone', 'camera', 'desktop', 'security', 'chat', 'sip', 'dialpad', 'settings', 'hangup', 'filmstrip'], // jshint ignore:line
+    //SETTINGS_SECTIONS: ['language', 'devices','moderator'],
     SETTINGS_SECTIONS: ['language', 'devices'],
     // Determines how the video would fit the screen. 'both' would fit the whole
     // screen, 'height' would fit the original video height to the height of the
